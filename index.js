c450efd4f1dac541748a16b71d7042c6b0657547
Martin <souzam@sonic.net>
2022-10-11 19:55:27 -0400

    commit tree 3e95c023a44be79b414810e66fd17a0fa688ea94

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction (cb) {
+    return cb()
+}
+
+function returnsANamedFunction() {
+    return receivesAFunction
+}
+
+function returnsAnAnonymousFunction() {
+    return (n) => n++
+}
